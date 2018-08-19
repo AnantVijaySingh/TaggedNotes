d66b72a40b2dc60c2bed5f64f68bbd1203da25c0
anantvijay.23@gmail.com <anantvijay.23@gmail.com>
2018-08-19 22:05:03 +0100

    Project Setup using create-react-native-app

---
+++ components/NewNote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+class NewNote extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>New Note</Text>
+            </View>
+        );
+    }
+}
+
+export default NewNote;
