4bc55962d01fb56456732356e6505749faaa3089
AnantVijaySingh <anantvijay@oath.com>
2018-09-09 21:52:00 -0700

    List View - Note design

--- components/List.js
+++ components/List.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableNativeFeedback, FlatList, TouchableOpacity, Platform } from 'react-native';
-import {green, grey2, white} from "../helpers/colors";
+import {green, grey2, grey3, white} from "../helpers/colors";
 import {connect} from 'react-redux';
 import {handleInitialData} from "../actions/shared";
 
@@ -32,12 +32,14 @@ class List extends React.Component {
                             style={styles.note}
                             onPress={() => {} }
                         >
-                            <View>
-                                <Text>{item['item']['heading']}</Text>
+                            <View style={{flex: 1}}>
+                                <Text style={styles.noteHeading}>{item['item']['heading']}</Text>
                             </View>
-                            <View>
+                            <View style={styles.noteTagsRow}>
                                 { item['item']['Tags'].map((item, key)=>(
-                                    <Text key={key}> { item } </Text>)
+                                    <View style={styles.noteTags}>
+                                        <Text key={key}> { item } </Text>
+                                    </View>)
                                 )}
                             </View>
                         </TouchableOpacity>
@@ -67,10 +69,26 @@ const styles = StyleSheet.create({
         backgroundColor: white,
         alignSelf: 'stretch',
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'flex-start',
         height: 100,
         marginTop: 10
     },
+    noteHeading: {
+        fontWeight: '600',
+        fontSize: 16,
+    },
+    noteTagsRow: {
+        flexDirection: 'row',
+    },
+    noteTags: {
+        borderWidth: 1,
+        borderRadius: 3,
+        borderStyle: 'solid',
+        borderColor: grey3,
+        margin: 2,
+        padding: 2,
+
+    }
 });
 
 function mapStateToProps(state) {
